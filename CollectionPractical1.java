8e61cc29c55b7ed135df1c73766b7a6f061ace50
SlDimi3ROX <shanalsilva296@gmail.com>
2024-10-19 21:06:25 +0530

    Add files via upload
    Data structures in class

---
+++ CollectionPractical1.java
@@ -0,0 +1,26 @@
+import java.util.Collection;
+import java.util.LinkedList;
+
+
+
+
+
+class CollectionPractical1{
+    public CollectionPractical1(){
+		Collection<Integer> c = new LinkedList();
+		c.add(5);
+		c.add(3);
+		c.add(5);
+		c.add(2);
+		c.add(4);
+		boolean isEmpty();
+		
+		
+	
+	}
+	public static void main(String []args){
+	
+	new CollectionPractical1();
+	}
+}
+	
